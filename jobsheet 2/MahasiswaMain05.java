ab65959994d7454bed02ee8ac0919b0f2cfb0951
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2025-02-20 08:37:30 +0700

    praktikum 2

---
+++ jobsheet 2/MahasiswaMain05.java
@@ -0,0 +1,15 @@
+public class MahasiswaMain05 {
+    public static void main(String[] args) {
+        
+        Mahasiswa05 mhs1 = new Mahasiswa05 ();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.NIM = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.5;
+
+        mhs1.tampikanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIPK(3.60);
+        mhs1.tampikanInformasi();
+    }
+}
